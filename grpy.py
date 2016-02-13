b7c5cea6ed67faef953deab3e5eb2c018bf0f197
Andrew Yates <andrew.yates@gmail.com>
2016-02-12 21:09:26 -0500

    basic

--- grpy.py
+++ grpy.py
@@ -3,5 +3,7 @@ import pygit2
 repo = pygit2.Repository('/home/andrew/dev/grpy')
 
 modified = len(repo.status())
+ahead, behind = repo.ahead_behind(repo.head.target, repo.lookup_branch('master').upstream.target)
 
-'git status --branch --porcelain'
+print "%s modified, %s ahead, %s behind" % (modified, ahead, behind)
+
